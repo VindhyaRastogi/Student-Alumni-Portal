38191133b08b1943157c4c844ce8691a486a2cc7
vindhyarastogi3 <vindhyarastogi3@iiitd.edu.in>
2025-08-25 17:03:28 +0530

    login buttons

--- student-alumni-frontend/src/pages/alumni/AlumniDashboard.jsx
+++ student-alumni-frontend/src/pages/alumni/AlumniDashboard.jsx
@@ -1,13 +1,33 @@
-import '../Dashboard.css';
-import { Link } from 'react-router-dom';
+import "../Dashboard.css";
+import { Link } from "react-router-dom";
 
 const AlumniDashboard = () => {
   const alumniMenu = [
-    { title: "Chats", link: "/alumni/chats", img: "https://cdn-icons-png.flaticon.com/512/2950/2950611.png" },
-    { title: "Meetings", link: "/alumni/meetings", img: "https://cdn-icons-png.flaticon.com/512/3094/3094853.png" },
-    { title: "History", link: "/alumni/history", img: "https://cdn-icons-png.flaticon.com/512/942/942748.png" },
-    { title: "Your Profile", link: "/alumni/profile", img: "https://cdn-icons-png.flaticon.com/512/1077/1077012.png" },
-    { title: "Report / Block", link: "/alumni/report", img: "https://cdn-icons-png.flaticon.com/512/484/484662.png" }
+    {
+      title: "Chats",
+      link: "/alumni/chats",
+      img: "https://cdn-icons-png.flaticon.com/512/2950/2950611.png",
+    },
+    {
+      title: "Meetings",
+      link: "/alumni/meetings",
+      img: "https://cdn-icons-png.flaticon.com/512/3094/3094853.png",
+    },
+    {
+      title: "History",
+      link: "/alumni/history",
+      img: "https://cdn-icons-png.flaticon.com/512/942/942748.png",
+    },
+    {
+      title: "Your Profile",
+      link: "/alumni/edit-profile",
+      img: "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+    },
+    {
+      title: "Report / Block",
+      link: "/alumni/report",
+      img: "https://cdn-icons-png.flaticon.com/512/484/484662.png",
+    },
   ];
 
   return (
